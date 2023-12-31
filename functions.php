cc1f74456cb112491db59b0d77f150d6fb274bd6
CyberPanel <support@cyberpanel.net>
2023-12-31 07:23:38 +0100

    design library

--- functions.php
+++ functions.php
@@ -21,3 +21,6 @@ require_once THEME_DIR . '/inc/dividers.php';
 
 // Load reusable dependencies
 require_once THEME_DIR . '/inc/reusable-dependencies.php';
+
+// Load design library
+require_once THEME_DIR . '/inc/design-library.php';
