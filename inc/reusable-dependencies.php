e234e740767acb14d781894afdcddfa4ee2cb416
CyberPanel <support@cyberpanel.net>
2024-01-19 20:06:37 +0100

    add comment to reusable dependencies

--- inc/reusable-dependencies.php
+++ inc/reusable-dependencies.php
@@ -1,4 +1,8 @@
 <?php 
+
+// This snippet register or change reusable dependencies
+// %%BREAKDANCE_REUSABLE_GSAP%%
+// %%BREAKDANCE_REUSABLE_SCROLL_TRIGGER%%
 add_action('breakdance_reusable_dependencies_urls', function ($urls) {
     $urls['gsap'] = THEME_URI . '/assets/lib/gsap.min.js';
     $urls['scrollTrigger'] = THEME_URI . '/assets/lib/ScrollTrigger.min.js';
