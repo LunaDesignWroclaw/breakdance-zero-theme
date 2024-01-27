22ad3558bc61273180d31cfaf9248f917f7e8ca1
Sławomir Oruba <slawomir.oruba@gmail.com>
2024-01-27 08:56:25 +0100

    appsero run

--- functions.php
+++ functions.php
@@ -9,6 +9,29 @@ define('THEME_URI', get_template_directory_uri());
 // Breakdance form actions directory
 define('BREAKDANCE_FORM_ACTIONS_DIR', THEME_DIR . '/inc/breakdance-form-actions');
 
+
+/**
+ * Initialize the plugin tracker
+ *
+ * @return void
+ */
+function appsero_init_tracker_lunadesign_zero_theme() {
+
+    if ( ! class_exists( 'Appsero\Client' ) ) {
+      require_once __DIR__ . '/appsero/src/Client.php';
+    }
+
+    $client = new Appsero\Client( '5b83a102-67ce-48ed-ac5f-3cf447f0c370', 'Breakdance Zero Theme', __FILE__ );
+
+    // Active insights
+    $client->insights()->init();
+
+    $client->updater();
+
+}
+
+appsero_init_tracker_lunadesign_zero_theme();
+
 // This is a theme for Breakdance. Check if Breakdance is enabled and if so, load the theme.
 require_once THEME_DIR . '/inc/breakdance-init.php';
 
