c6c0ff068a034fdbece3586aa83c19a60cd61fcc
Sławomir Oruba <slawomir.oruba@gmail.com>
2024-07-20 15:20:54 +0000

    Fix bug - fatal error on update

--- functions.php
+++ functions.php
@@ -44,19 +44,23 @@ function enqueue_theme_scripts() {
 
 // Check for theme updates
 add_filter('site_transient_update_themes', 'breakdance_zero_theme_update_themes');
-function breakdance_zero_theme_update_themes($transient) {
 
-    // let's get the theme directory name
+function breakdance_zero_theme_update_themes($transient) {
+    // Ensure $transient is an object
+    if (!is_object($transient)) {
+        $transient = new stdClass();
+    }
+
+    // Let's get the theme directory name
     $stylesheet = get_template();
 
-    // now let's get the theme version
+    // Now let's get the theme version
     $theme = wp_get_theme();
     $version = $theme->get('Version');
 
-    // check if we have a cached response
+    // Check if we have a cached response
     if (false == $remote = get_transient('breakdance-zero-theme-update-' . $version)) {
-
-        // connect to a remote server where the update information is stored
+        // Connect to a remote server where the update information is stored
         $remote = wp_remote_get(
             'https://breakdance-zero-theme.cytr.us/info.php',
             array(
@@ -67,7 +71,7 @@ function breakdance_zero_theme_update_themes($transient) {
             )
         );
 
-        // do nothing if errors
+        // Do nothing if errors
         if (
             is_wp_error($remote)
             || 200 !== wp_remote_retrieve_response_code($remote)
@@ -79,10 +83,10 @@ function breakdance_zero_theme_update_themes($transient) {
         $remote = json_decode(wp_remote_retrieve_body($remote));
 
         if (!$remote) {
-            return $transient; // who knows, maybe JSON is not valid
+            return $transient; // Who knows, maybe JSON is not valid
         }
 
-        // cache the response for 1 hour
+        // Cache the response for 1 hour
         set_transient('breakdance-zero-theme-update-' . $version, $remote, HOUR_IN_SECONDS);
     }
 
@@ -95,7 +99,7 @@ function breakdance_zero_theme_update_themes($transient) {
         'package' => $remote->download_url,
     );
 
-    // check all the versions now
+    // Check all the versions now
     if (
         $remote
         && version_compare($version, $remote->version, '<')
@@ -109,3 +113,4 @@ function breakdance_zero_theme_update_themes($transient) {
 
     return $transient;
 }
+?>
